11e2b6eeae759814cb13a8dc5cc3df1ea9102384
Harrison8888 <oliver_harrison22@163.com>
2024-08-08 11:17:23 +0900

    Create func_py_calculate_rectangular_prism_surface_area.py

---
+++ Codes/Python/func_py_calculate_rectangular_prism_surface_area.py
@@ -0,0 +1,5 @@
+# func_py_calculate_rectangular_prism_surface_area.py
+def func_py_calculate_rectangular_prism_surface_area(length, width, height):
+    return 2 * (length * width + width * height + length * height)
+
+print(func_py_calculate_rectangular_prism_surface_area(3, 4, 5))
