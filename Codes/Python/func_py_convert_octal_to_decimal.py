2860c62ca554d9891d95ce63d8a05d8e388c1137
Harrison8888 <oliver_harrison22@163.com>
2024-08-09 10:32:10 +0900

    Create func_py_convert_octal_to_decimal.py

---
+++ Codes/Python/func_py_convert_octal_to_decimal.py
@@ -0,0 +1,9 @@
+# func_py_convert_octal_to_decimal.py
+def func_py_convert_octal_to_decimal(octal_str):
+    try:
+        return int(octal_str, 8)
+    except ValueError:
+        return None
+
+print(func_py_convert_octal_to_decimal('17'))
+print(func_py_convert_octal_to_decimal('20'))
