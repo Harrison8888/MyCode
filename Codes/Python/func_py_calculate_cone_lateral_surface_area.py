920cb44411b2b25748e33e8984238f7432a25828
Harrison8888 <oliver_harrison22@163.com>
2024-08-11 16:00:55 +0900

    Create func_py_calculate_cone_lateral_surface_area.py

---
+++ Codes/Python/func_py_calculate_cone_lateral_surface_area.py
@@ -0,0 +1,7 @@
+# func_py_calculate_cone_lateral_surface_area.py
+import math
+
+def func_py_calculate_cone_lateral_surface_area(radius, slant_height):
+    return math.pi * radius * slant_height
+
+print(func_py_calculate_cone_lateral_surface_area(3, 5))
