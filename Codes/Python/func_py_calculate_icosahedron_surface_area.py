b7b7fcc5accd6f441ad3c462b54e87ab03934851
Harrison8888 <oliver_harrison22@163.com>
2024-08-27 17:50:00 +0900

    Create func_py_calculate_icosahedron_surface_area.py

---
+++ Codes/Python/func_py_calculate_icosahedron_surface_area.py
@@ -0,0 +1,7 @@
+# func_py_calculate_icosahedron_surface_area.py
+import math
+
+def func_py_calculate_icosahedron_surface_area(side_length):
+    return 5 * math.sqrt(3) * side_length**2
+
+print(func_py_calculate_icosahedron_surface_area(4))
