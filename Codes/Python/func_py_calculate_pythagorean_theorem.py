2c40fd28e86ac46aebac410796e8e1b495a8f825
Harrison8888 <oliver_harrison22@163.com>
2024-09-24 23:00:01 +0900

    Create func_py_calculate_pythagorean_theorem.py

---
+++ Codes/Python/func_py_calculate_pythagorean_theorem.py
@@ -0,0 +1,5 @@
+# func_py_calculate_pythagorean_theorem.py
+def func_py_calculate_pythagorean_theorem(a, b):
+    return (a ** 2 + b ** 2) ** 0.5
+
+print(func_py_calculate_pythagorean_theorem(3, 4))
