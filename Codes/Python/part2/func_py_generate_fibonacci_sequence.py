79ccc70bfecbf7021b66a274d47f8926f40f83c0
Harrison8888 <oliver_harrison22@163.com>
2024-10-09 23:50:47 +0900

    Create func_py_generate_fibonacci_sequence.py

---
+++ Codes/Python/part2/func_py_generate_fibonacci_sequence.py
@@ -0,0 +1,6 @@
+# func_py_generate_fibonacci_sequence.py
+def func_py_generate_fibonacci_sequence(n):
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence[:n]
